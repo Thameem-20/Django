2c49910475f6445c732d73b86cbbbd2cb73faddb
Thameem-20 <ahmedthameem20@gmail.com>
2024-05-15 22:13:25 +0530

    updated views

--- testproject/home/views.py
+++ testproject/home/views.py
@@ -7,3 +7,8 @@ def index(request):
 def about(request):
     return HttpResponse('This is about')
 
+def services(request):
+    return HttpResponse('This is services')\
+
+def contact(request):
+    return HttpResponse('This is contact')
