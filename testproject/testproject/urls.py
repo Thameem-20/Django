b6dd421ad556b2d7218483fd66650cd6d39738d1
Thameem-20 <ahmedthameem20@gmail.com>
2024-05-30 22:50:49 +0530

    new commit

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Thameem Admin"
+admin.site.site_title = "Thameem Portfolio Admin Portal"
+admin.site.index_title = "Welcome to Thameem's Portfolio"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
